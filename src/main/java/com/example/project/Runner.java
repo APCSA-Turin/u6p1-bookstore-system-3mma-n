b066f88890c899450be7265caa67c457c5e16164
3mma-n <148897839+3mma-n@users.noreply.github.com>
2025-01-17 17:34:29 +0000

    1/17

--- src/main/java/com/example/project/Runner.java
+++ src/main/java/com/example/project/Runner.java
@@ -1,7 +1,20 @@
 package com.example.project;
-
+import java.util.Scanner;
 public class Runner {
+    private static void clear() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public static void main(String[] args) {
-        System.out.println();
+        int input = 0;
+        Scanner sc = new Scanner(System.in);
+        while (input != -1) {
+            clear();
+            System.out.print("Enter Choice: ");
+            input = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Selected Choice " + input);
+        }
+        sc.close();
     }
 }
